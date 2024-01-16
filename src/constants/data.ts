9836c52755439c1dad50426557c5cf8f27441ffb
naomipham <phambaonguyendn@gmail.com>
2024-01-16 11:22:19 +0700

    reset

---
+++ src/constants/data.ts
@@ -0,0 +1,25 @@
+const PoemData = [
+  {
+    id: 0,
+    title: "The Happiness",
+    author: "The Happiness",
+    content:
+      "<p>There's a happiness, a joy <br/> in one soul, that's been <br/> buried alive in everyone <br/> and forgotten. <br/> <br/> It isn't your barroom joke <br/> or tender, intimate humor <br/> or affections of friendliness <br/> or big, bright pun. <br/> <br/> They're the surviving survivors <br/> of what happened when happiness <br/> was buried alive, when <br/> it no longer looked out <br/> <br/> of today's eyes, and doesn't <br/> even manifest when one <br/> of us dies, we just walk away <br/> from everything, alone <br/> <br/> with what's left of us, <br/> going on being human beings <br/> without being human, <br/> without that happiness. <br/> </p>",
+  },
+  {
+    id: 1,
+    title: "The Happiness",
+    author: "The Happiness",
+    content:
+      "<p>There's a happiness, a joy <br/> in one soul, that's been <br/> buried alive in everyone <br/> and forgotten. <br/> <br/> It isn't your barroom joke <br/> or tender, intimate humor <br/> or affections of friendliness <br/> or big, bright pun. <br/> <br/> They're the surviving survivors <br/> of what happened when happiness <br/> was buried alive, when <br/> it no longer looked out <br/> <br/> of today's eyes, and doesn't <br/> even manifest when one <br/> of us dies, we just walk away <br/> from everything, alone <br/> <br/> with what's left of us, <br/> going on being human beings <br/> without being human, <br/> without that happiness. <br/> </p>",
+  },
+  {
+    id: 2,
+    title: "The Happiness",
+    author: "The Happiness",
+    content:
+      "<p>There's a happiness, a joy <br/> in one soul, that's been <br/> buried alive in everyone <br/> and forgotten. <br/> <br/> It isn't your barroom joke <br/> or tender, intimate humor <br/> or affections of friendliness <br/> or big, bright pun. <br/> <br/> They're the surviving survivors <br/> of what happened when happiness <br/> was buried alive, when <br/> it no longer looked out <br/> <br/> of today's eyes, and doesn't <br/> even manifest when one <br/> of us dies, we just walk away <br/> from everything, alone <br/> <br/> with what's left of us, <br/> going on being human beings <br/> without being human, <br/> without that happiness. <br/> </p>",
+  },
+];
+
+export default PoemData;
